4c1025f51b8f4ca004f6c28c32d149e4faa828e3
ujjwal <ujjwal.mishra@hcode.tech>
2025-06-03 15:47:58 +0530

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,59 @@
+import React, { useState } from 'react';
+import './App.css';
+function App() {
+  const [input, setInput] = useState("");
+  const [todos, setTodos] = useState([]);
+
+  const handleChange = (e) => {
+    setInput(e.target.value);
+  };
+
+  const handleAdd = () => {
+    if (input.trim() === "") return;
+    setTodos([...todos, input]);
+    setInput(""); 
+  };
+  const handleDelete=(index)=>{
+    const newTodos=todos.filter((_,i)=> i!==index);
+    setTodos(newTodos);
+    console.log(newTodos);
+  }
+  const handleComplete=(index)=>{
+    const newTodos=todos.map((task,i)=> i===index?{...task,completed:true}:task);
+    setTodos(newTodos);
+    console.log(newTodos);
+  }
+  
+
+  return (
+    <div>
+      <h1>Task Manager</h1>
+      <div className='container'>
+        <input
+          type='text'
+          placeholder='Add a new task'
+          value={input}
+          onChange={handleChange}
+        />
+        <button onClick={handleAdd}>Add</button>
+      </div>
+      <ul>
+
+        {todos.map((task, index) => (
+
+          <div key={index}>
+            
+          <li>{task}</li>
+           
+          <button onClick={()=>handleDelete(index)}>Delete</button> 
+
+        
+          </div>
+        
+        ))}
+      </ul> 
+    </div>
+  );
+}
+
+export default App;
